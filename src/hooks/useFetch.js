374a3da8a8ea4ee5c9d96b0ee634e624dfe6aa01
Josip Ivancic <ivancicjosip073@gmail.com>
2020-12-17 14:43:09 +0100

    fixes

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,34 +1,29 @@
-import Axios from 'axios';
+import axios from 'axios';
 import { reactive, watch } from 'vue';
 
 export default function useFetch(url) {
-	const state = reactive({
-		data: [],
-		error: null,
-		isLoading: false,
-	});
+    const state = reactive({
+        data: [],
+        error: null,
+        isLoading: false,
+    });
 
-	const fetchData = async () => {
-		try {
-			state.error = null;
-			state.isLoading = true;
-			const { data } = await Axios.get(url.value);
-			console.log(data);
-			state.data = data;
-			state.isLoading = false;
-		} catch (error) {
-			state.isLoading = false;
-			state.error = 'Something went wrong';
-			console.error(error);
-		}
-	};
+    const fetchData = async () => {
+        try {
+            state.error = null;
+            state.isLoading = true;
+            const { data } = await axios.get(url.value);
+            console.log(data);
+            state.data = data;
+            state.isLoading = false;
+        } catch (error) {
+            state.isLoading = false;
+            state.error = 'Something went wrong';
+            console.error(error);
+        }
+    };
 
-	watch(
-		url,
+    watch(url, fetchData, { immediate: true });
 
-		fetchData,
-		{ immediate: true },
-	);
-
-	return state;
+    return state;
 }
